d4bf0705733b7a479aef67d6a9d842d6e8207bdc
sush2jan <sushovan2jan@gmail.com>
2020-12-30 20:53:22 +0530

    changed in the mapping path

--- src/main/java/com/ktree/helloworld/restcontroller/ErrorHandlerController.java
+++ src/main/java/com/ktree/helloworld/restcontroller/ErrorHandlerController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ErrorHandlerController implements ErrorController{
-	private static final String PATH = "/";
+	private static final String PATH = "/demo";
 	 @RequestMapping(value=PATH)
 	 public String error() {
 	  return "Welcome to release demo :)";
